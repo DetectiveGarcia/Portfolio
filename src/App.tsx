9bb8ade35fc2e7b78156b73ab78ff32fb2a1a312
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-14 20:13:19 +0100

    feat(navbar): Implemented navbar in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import SectionLayout from "./layouts/SectionLayout"
+import SectionLayout from "./layouts/SectionLayout";
+import NavBar from "./navbar/NavBar";
 
 const App = () => {
   return (
-    <SectionLayout sectionId={"app-layout"}>
-      <div>App</div>
-    </SectionLayout>
-  )
-}
+    <>
+      <NavBar />
+      <SectionLayout sectionId={"app-layout"}>
+        <div>App</div>
+      </SectionLayout>
+    </>
+  );
+};
 
-export default App
+export default App;
